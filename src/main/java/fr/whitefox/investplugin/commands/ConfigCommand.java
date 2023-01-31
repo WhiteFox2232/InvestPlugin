7f6aeceb891319cbf1f54b863d46d207c93b3384
WhiteFox <53935741+WhiteFox2232@users.noreply.github.com>
2023-01-31 10:52:28 +0100

    ✨ New TabComplete system for ConfigCommand

--- src/main/java/fr/whitefox/investplugin/commands/ConfigCommand.java
+++ src/main/java/fr/whitefox/investplugin/commands/ConfigCommand.java
@@ -8,47 +8,55 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ConfigCommand implements CommandExecutor, TabCompleter {
 
-    private static final String[] COMMANDS = {"area"};
+    private static final String[] ARGS_BASE = {"area"};
+    private static final String[] ARGS_AREA = {"set", "get"};
     private static final String AREA = "area";
+    private static final String GET = "get";
+    private static final String SET = "set";
     FileConfiguration config = Main.getInstance().getConfig();
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        final List<String> completions = new ArrayList<>();
-        StringUtil.copyPartialMatches(args[0], Arrays.asList(COMMANDS), completions);
-        Collections.sort(completions);
-        return completions;
+        if (args.length == 1) {
+            return new ArrayList<>(Arrays.asList(ARGS_BASE));
+        } else if (args.length == 2) {
+            return new ArrayList<>(Arrays.asList(ARGS_AREA));
+        }
+
+        return null;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
-        if (args.length == 0) {
-            sender.sendMessage(Message.CHAT_BAD_ARGS_CONFIG);
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Message.ERROR_NOT_A_PLAYER);
             return false;
         }
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(Message.ERROR_NOT_A_PLAYER);
+        if (args.length != 2) {
+            sender.sendMessage(Message.CHAT_BAD_ARGS_CONFIG);
             return false;
         }
 
         Player player = (Player) sender;
 
         if (args[0].equalsIgnoreCase(AREA)) {
-            configureArea(player);
-            return true;
+            if (args[1].equalsIgnoreCase(GET)) {
+                getArea(player);
+                return true;
+            } else if (args[1].equalsIgnoreCase(SET)) {
+                configureArea(player);
+                return true;
+            }
         }
 
-        player.sendMessage(Message.CHAT_BAD_ARGS_CONFIG);
         return false;
     }
 
@@ -56,5 +64,9 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
         player.sendMessage(Message.CHAT_COORDINATES + player.getLocation().getChunk().getX() + "," + player.getLocation().getChunk().getZ());
         config.set("area.x", player.getLocation().getChunk().getX());
         config.set("area.z", player.getLocation().getChunk().getZ());
+    }
+
+    public void getArea(Player player) {
+        player.sendMessage(Message.CHAT_COORDINATES + config.getInt("area.x") + "," + config.getInt("area.z"));
     }
 }
